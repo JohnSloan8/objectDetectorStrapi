f941d6fee1bdf1eac0b9384c45936962e332bff2
JohnSloan8 <johnsloan1984@gmail.com>
2022-03-29 11:32:46 +0100

    added heroku database name password etc. again

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: "ec2-63-35-156-160.eu-west-1.compute.amazonaws.com",
+      port: 5432,
+      database: "duk1ri1n5i57b",
+      user: "gmuxupkwrxnijo",
+      password: "8fc48ac103a7dc3a689726a28391e242ec9bc9c03b7c48245c6e1de91d813217",
       ssl: {
         rejectUnauthorized: false
       },
